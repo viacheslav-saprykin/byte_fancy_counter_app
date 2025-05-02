4ca345e9124d1aabd18299d50f63cf04ff11a641
Viacheslav Saprykin <saprykinvp@gmail.com>
2025-05-02 13:05:53 +0300

    add: function for reset buttons

--- src/Card.jsx
+++ src/Card.jsx
@@ -11,7 +11,7 @@ export default function Card() {
     <div className="card">
       <Title />
       <Count count={count} />
-      <ResetButton />
+      <ResetButton setCount={setCount} />
       <CountButtons setCount={setCount} />
     </div>
   );
--- src/ResetButton.jsx
+++ src/ResetButton.jsx
@@ -1,9 +1,13 @@
 import { ResetIcon } from "@radix-ui/react-icons";
 
-export default function ResetButton() {
+export default function ResetButton({ setCount }) {
   return (
-    <button className="reset-btn">
-    <ResetIcon className="reset-btn-icon"/>
+    <button 
+    onClick={() => {
+      setCount(0);
+    }}
+      className="reset-btn">
+        <ResetIcon className="reset-btn-icon"/>
     </button>
   )
 }

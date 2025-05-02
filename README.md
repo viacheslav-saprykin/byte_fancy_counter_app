214d67cefdbefa2b2ef186c1e07850d766efa27f
Viacheslav Saprykin <saprykinvp@gmail.com>
2025-05-02 11:55:20 +0300

    add: styles,lib radix-ui

---
+++ README.md
@@ -0,0 +1,4 @@
+# React
+# Vite
+# JavaScript
+# Radix UI
--- src/ResetButton.jsx
+++ src/ResetButton.jsx
@@ -1,4 +1,9 @@
-export default function ResetButton() {
-  return <button>Reset</button>
-}
+import { ResetIcon } from "@radix-ui/react-icons";
 
+export default function ResetButton() {
+  return (
+    <button className="reset-btn">
+    <ResetIcon className="reset-btn-icon"/>
+    </button>
+  )
+}
